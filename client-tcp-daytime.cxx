f4c0df47c1074f614f5be97e180acc1fc88010cd
TROUIN Nicolas <t17009199@a-maq-iutinfo.salsa.univ-amu.fr>
2019-03-05 15:45:18 +0100

    Exercice 2 - Client TCP daytime

---
+++ client-tcp-daytime.cxx
@@ -0,0 +1,59 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define NUM_PORT 50013
+#define IP_ADDRESS "10.203.9.212"
+#define MAX_SIZE 1024
+
+using namespace std;
+
+void exitErreur(const char * msg) {
+	perror(msg);
+	exit( EXIT_FAILURE);
+
+}//exitErreur
+
+int main(int argc, char* argv[]) {
+
+	if(argc != 3){
+		cerr << "Usage : ./client-tcp-daytime.run <IP@> <port>" << endl;
+		return -1;
+	}
+
+	cout << "IP = " << argv[1] << endl
+	     << "PORT = " << argv[2] << endl;
+
+	int sock_client = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in sockaddr_client, sockaddr_serveur;
+
+	sockaddr_serveur.sin_family = AF_INET;
+	sockaddr_serveur.sin_port = htons(atoi(argv[2]));
+	inet_aton(argv[1], &sockaddr_serveur.sin_addr);
+	
+	connect(sock_client, (struct sockaddr*)&sockaddr_serveur, sizeof(sockaddr_serveur));
+
+	string message = "";
+	char buf[MAX_SIZE];
+	while(true){
+		int result = read(sock_client, buf, sizeof(buf)-1);
+		if(result == -1)
+			exitErreur("read");
+		else if(result > 0){
+			buf[result] = 0x00;
+			message += buf;
+		}
+		else
+			break;
+	}
+
+	cout << message;
+
+	close(sock_client);
+
+}//main
