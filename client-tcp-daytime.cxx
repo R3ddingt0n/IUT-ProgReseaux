5095c838d42dc65fb4b624ad951c27dd1fb90c26
TROUIN Nicolas <t17009199@a-maq-iutinfo.salsa.univ-amu.fr>
2019-03-05 15:47:39 +0100

    Added comment

--- client-tcp-daytime.cxx
+++ client-tcp-daytime.cxx
@@ -26,8 +26,10 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
+	/*
 	cout << "IP = " << argv[1] << endl
 	     << "PORT = " << argv[2] << endl;
+	*/
 
 	int sock_client = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in sockaddr_client, sockaddr_serveur;
